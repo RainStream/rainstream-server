2d6f387af7d2be2eef478e834fc03fd900f88b43
ycj <345252622@qq.com>
2020-07-02 17:42:53 +0800

    完善配置

---
+++ Src/ClusterServer/config.cpp
@@ -0,0 +1,123 @@
+#include "common.hpp"
+
+#include "config.h"
+
+json config =
+R"(
+{
+	"domain" : "localhost",
+	"https" :
+	{
+		"listenIp"   : "0.0.0.0",
+		"listenPort" : 4443,
+		"tls"        :
+		{
+			"cert" : "./certs/fullchain.pem",
+			"key"  : "./certs/privkey.pem"
+		}
+	},
+	"mediasoup" :
+	{
+		"numWorkers" : 1,
+		"workerSettings" :
+		{
+			"logLevel" : "warn",
+			"logTags"  :
+			[
+				"info",
+				"ice",
+				"dtls",
+				"rtp",
+				"srtp",
+				"rtcp",
+				"rtx",
+				"bwe",
+				"score",
+				"simulcast",
+				"svc",
+				"sctp"
+			],
+			"rtcMinPort" : 40000,
+			"rtcMaxPort" : 49999
+		},
+		"routerOptions" :
+		{
+			"mediaCodecs" :
+			[
+				{
+					"kind"      : "audio",
+					"mimeType"  : "audio/opus",
+					"clockRate" : 48000,
+					"channels"  : 2
+				},
+				{
+					"kind"       : "video",
+					"mimeType"   : "video/VP8",
+					"clockRate"  : 90000,
+					"parameters" :
+					{
+						"x-google-start-bitrate" : 1000
+					}
+				},
+				{
+					"kind"       : "video",
+					"mimeType"   : "video/VP9",
+					"clockRate"  : 90000,
+					"parameters" :
+					{
+						"profile-id"             : 2,
+						"x-google-start-bitrate" : 1000
+					}
+				},
+				{
+					"kind"       : "video",
+					"mimeType"   : "video/h264",
+					"clockRate"  : 90000,
+					"parameters" :
+					{
+						"packetization-mode"      : 1,
+						"profile-level-id"        : "4d0032",
+						"level-asymmetry-allowed" : 1,
+						"x-google-start-bitrate"  : 1000
+					}
+				},
+				{
+					"kind"       : "video",
+					"mimeType"   : "video/h264",
+					"clockRate"  : 90000,
+					"parameters" :
+					{
+						"packetization-mode"      : 1,
+						"profile-level-id"        : "42e01f",
+						"level-asymmetry-allowed" : 1,
+						"x-google-start-bitrate"  : 1000
+					}
+				}
+			]
+		},
+		"webRtcTransportOptions" :
+		{
+			"listenIps" :
+			[
+				{
+					"ip"          : "127.0.0.1",
+					"announcedIp" : "127.0.0.1"
+				}
+			],
+			"initialAvailableOutgoingBitrate" : 1000000,
+			"minimumAvailableOutgoingBitrate" : 600000,
+			"maxSctpMessageSize"              : 262144,
+			"maxIncomingBitrate"              : 1500000
+		},
+
+		"plainTransportOptions" :
+		{
+			"listenIp" :
+			{
+				"ip"          : "127.0.0.1",
+				"announcedIp" : "127.0.0.1"
+			},
+			"maxSctpMessageSize" : 262144
+		}
+	}
+})"_json;
