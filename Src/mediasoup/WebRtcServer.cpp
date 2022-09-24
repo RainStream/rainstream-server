d48a20d25da69e67b5f05480b5040c648d1a7be0
HarvestSure <admin@gupar.com>
2022-09-24 15:31:39 +0800

    WebRtcServer

--- Src/mediasoup/WebRtcServer.cpp
+++ Src/mediasoup/WebRtcServer.cpp
@@ -1,1 +1,123 @@
+#define MSC_CLASS "WebRtcServer"
+
+#include "common.hpp"
 #include "WebRtcServer.hpp"
+#include "WebRtcTransport.hpp"
+#include "Channel.hpp"
+#include "errors.hpp"
+#include "Logger.hpp"
+
+
+WebRtcServer::WebRtcServer(const json& internal, Channel* channel, const json& appData)
+	: EnhancedEventEmitter()
+	, _internal(internal)
+	, _channel(channel)
+	, _appData(appData)
+{
+	MSC_DEBUG("constructor()");
+}
+
+std::string WebRtcServer::id()
+{
+	return this->_internal["webRtcServerId"];
+}
+
+bool WebRtcServer::closed()
+{
+	return this->_closed;
+}
+
+json WebRtcServer::appData()
+{
+	MSC_THROW_ERROR("cannot override appData object");
+}
+
+void WebRtcServer::appData(json appData)
+{
+	this->_appData = appData;
+}
+
+std::map<std::string, WebRtcTransport*> WebRtcServer::webRtcTransportsForTesting()
+{
+	return this->_webRtcTransports;
+}
+
+void WebRtcServer::close()
+{
+	if (this->_closed)
+		return;
+
+	MSC_DEBUG("close()");
+
+	this->_closed = true;
+
+	json reqData = { { "webRtcServerId", this->_internal["webRtcServerId"]}};
+
+	try
+	{
+		this->_channel->request("worker.closeWebRtcServer", undefined, reqData);
+	}	
+	catch (const std::exception&)
+	{
+
+	}
+
+	// Close every WebRtcTransport.
+	//for (const webRtcTransport of this->_webRtcTransports.values())
+	//{
+	//	webRtcTransport.listenServerClosed();
+
+	//	// Emit observer event.
+	//	this->_observer.safeEmit("webrtctransportunhandled", webRtcTransport);
+	//}
+	this->_webRtcTransports.clear();
+
+	this->emit("@close");
+
+	// Emit observer event.
+	//this->_observer.safeEmit("close");
+}
+
+void WebRtcServer::workerClosed()
+{
+	if (this->_closed)
+		return;
+
+	MSC_DEBUG("workerClosed()");
+
+	this->_closed = true;
+
+	// NOTE: No need to close WebRtcTransports since they are closed by their
+	// respective Router parents.
+	this->_webRtcTransports.clear();
+
+	this->safeEmit("workerclose");
+
+	// Emit observer event.
+	//this->_observer.safeEmit("close");
+}
+
+std::future<json> WebRtcServer::dump()
+{
+	MSC_DEBUG("dump()");
+
+	json ret = co_await this->_channel->request("webRtcServer.dump", this->_internal["webRtcServerId"]);
+
+	co_return ret;
+}
+
+void WebRtcServer::handleWebRtcTransport(WebRtcTransport* webRtcTransport)
+{
+	this->_webRtcTransports.insert(std::pair(webRtcTransport->id(), webRtcTransport));
+
+	// Emit observer event.
+	//this->_observer.safeEmit("webrtctransporthandled", webRtcTransport);
+
+	webRtcTransport->on("@close", [=]()
+	{
+		this->_webRtcTransports.erase(webRtcTransport->id());
+
+		// Emit observer event.
+		//this->_observer.safeEmit("webrtctransportunhandled", webRtcTransport);
+	});
+}
--- Src/mediasoup/WebRtcServer.hpp
+++ Src/mediasoup/WebRtcServer.hpp
@@ -1,6 +1,118 @@
 #pragma once
 
-class WebRtcServer
+#include "EnhancedEventEmitter.hpp"
+#include "Transport.hpp"
+
+class Channel;
+class WebRtcTransport;
+
+struct WebRtcServerListenInfo
 {
+	/**
+	 * Network protocol.
+	 */
+	TransportProtocol protocol;
+
+	/**
+	 * Listening IPv4 or IPv6.
+	 */
+	std::string ip;
+
+	/**
+	 * Announced IPv4 or IPv6 (useful when running mediasoup behind NAT with
+	 * private IP).
+	 */
+	std::optional<std::string> announcedIp;
+
+	/**
+	 * Listening port.
+	 */
+	uint16_t port;
 };
 
+struct WebRtcServerOptions
+{
+	/**
+	 * Listen infos.
+	 */
+	std::list<WebRtcServerListenInfo> listenInfos;
+
+	/**
+	 * Custom application data.
+	 */
+	json appData;
+};
+
+
+class WebRtcServer : public EnhancedEventEmitter
+{
+public:
+	WebRtcServer(const json& internal, Channel* channel, const json& appData);
+
+	/**
+	 * WebRtcServer id.
+	 */
+	std::string id();
+
+	/**
+	 * Whether the WebRtcServer is closed.
+	 */
+	bool closed();
+
+	/**
+	 * App custom data.
+	 */
+	json appData();
+
+	/**
+	 * Invalid setter.
+	 */
+	void appData(json appData);
+
+	/**
+	 * @private
+	 * Just for testing purposes.
+	 */
+	std::map<std::string, WebRtcTransport*> webRtcTransportsForTesting();
+
+	/**
+	 * Close the WebRtcServer.
+	 */
+	void  close();
+
+	/**
+	 * Worker was closed.
+	 *
+	 * @private
+	 */
+	void workerClosed();
+
+	/**
+	 * Dump WebRtcServer.
+	 */
+	std::future<json> dump();
+
+	/**
+	 * @private
+	 */
+	void  handleWebRtcTransport(WebRtcTransport* webRtcTransport);
+
+private:
+	// Internal data.
+	json _internal;
+
+	// Channel instance.
+	Channel* _channel;
+
+	// Closed flag.
+	bool _closed = false;
+
+	// Custom app data.
+	json _appData;
+
+	// Transports map.
+	std::map<std::string, WebRtcTransport*> _webRtcTransports;
+
+	// Observer instance.
+	//observer = new EnhancedEventEmitter<WebRtcServerObserverEvents>();
+};
