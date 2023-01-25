423ad5a496bbed0433fa7ec60edf9e24d4e81965
yangcj <gupar@qq.com>
2023-01-25 15:43:49 +0800

    完善

--- Src/ClusterServer/config.cpp
+++ Src/ClusterServer/config.cpp
@@ -90,13 +90,13 @@ R"(
 				{
 					"protocol": "udp",
 					"ip": "0.0.0.0",
-					"announcedIp": "192.168.0.103",
+					"announcedIp": "192.168.1.15",
 					"port": 44444
 				},
 				{
 					"protocol": "tcp",
 					"ip": "0.0.0.0",
-					"announcedIp": "192.168.0.103",
+					"announcedIp": "192.168.1.15",
 					"port": 44444
 				}
 			]
@@ -105,7 +105,7 @@ R"(
 			"listenIps": [
 				{
 					"ip": "0.0.0.0",
-					"announcedIp": "192.168.0.103"
+					"announcedIp": "192.168.1.15"
 				}
 			],
 			"initialAvailableOutgoingBitrate": 1000000,
@@ -116,7 +116,7 @@ R"(
 		"plainTransportOptions": {
 			"listenIp": {
 				"ip": "0.0.0.0",
-				"announcedIp": "192.168.0.103"
+				"announcedIp": "192.168.1.15"
 			},
 			"maxSctpMessageSize": 262144
 		}
